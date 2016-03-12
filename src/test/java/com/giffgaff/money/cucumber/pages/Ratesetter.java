f734bf63fe7ca410b751e554df607cbd92549f9b
Esther Lloyd <esther@giffgaff.co.uk>
2016-03-12 13:08:55 +0000

    Ratesetter quote form load verification

--- src/test/java/com/giffgaff/money/cucumber/pages/Ratesetter.java
+++ src/test/java/com/giffgaff/money/cucumber/pages/Ratesetter.java
@@ -1,13 +1,21 @@
 package com.giffgaff.money.cucumber.pages;
 
-//import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.support.FindBy;
+
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.NamedUrl;
 import net.thucydides.core.annotations.NamedUrls;
 import net.thucydides.core.pages.PageObject;
 
-@NamedUrls({ @NamedUrl(name = "money", url = "{1}/peer-2-peer-lending?dtp=1") })
+@NamedUrls({ @NamedUrl(name = "money", url = "https://peer2peer.giffgaff.com/your_borrowing/applications/quote.aspx") })
 public class Ratesetter extends PageObject {
-
+	
+	@FindBy(css = "h1")
+	private WebElementFacade quotePageHeader;
+	
+	public String quoteHeaderText(){
+		return quotePageHeader.getText();
+	}
 }
 
 
